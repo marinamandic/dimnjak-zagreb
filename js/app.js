106c190e553c628b27206308280ba43eaf907d3a
marinamandic <marina_rukavina@yahoo.com>
2015-11-29 21:25:32 +0100

    Pushed finished project. Missing "usluge".

---
+++ js/app.js
@@ -0,0 +1,17 @@
+﻿// inits foundations js components
+$(document).foundation();
+
+$(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        paginationClickable: true,
+        // If we need pagination
+        pagination: '.swiper-pagination',
+
+        // Navigation arrows
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev'
+    })
+});
